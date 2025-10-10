459cce65a38b06175ebc963347a58c0c20339222
lien <quynhlien44@gmail.com>
2025-10-10 16:28:39 +0300

    Exercise: 2.1 - 2.15

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,91 @@
+import { useState, useEffect } from 'react'
+import personService from './services/persons'
+
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(initialPersons => {
+        console.log('Fetched:', initialPersons)
+        setPersons(initialPersons)
+      })
+  }, [])
+
+  const handleNameChange = (event) => setNewName(event.target.value)
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
+  const handleFilterChange = (event) => setFilter(event.target.value)
+
+  const handleDelete = (id, name) => {
+    if (window.confirm(`Delete ${name} ?`)) {
+      personService
+        .remove(id)
+        .then(() => {
+          setPersons(persons.filter(person => person.id !== id))
+        })
+    }
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const existingPerson = persons.find(person => person.name === newName)
+    if (existingPerson) {
+      if (window.confirm(
+        `${existingPerson.name} is already added to phonebook, replace the old number with a new one?`
+      )) {
+        const changedPerson = { ...existingPerson, number: newNumber }
+        personService
+          .update(existingPerson.id, changedPerson)
+          .then(returnedPerson => {
+            setPersons(persons.map(p => p.id !== existingPerson.id ? p : returnedPerson))
+            setNewName('')
+            setNewNumber('')
+          })
+      }
+      return
+    }
+
+    const personObject = { name: newName, number: newNumber }
+    personService
+      .create(personObject)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+        setNewName('')
+        setNewNumber('')
+      })
+  }
+
+
+  const personsToShow = persons.filter(person =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  )
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter value={filter} onChange={handleFilterChange} />
+
+      <h2>add a new</h2>
+      <PersonForm
+        onSubmit={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+
+      <h2>Numbers</h2>
+      <Persons persons={personsToShow} onDelete={handleDelete} />
+    </div>
+  )
+}
+
+export default App
