5b6ec1c404872642c985378f6f00d696e535a036
lien <quynhlien44@gmail.com>
2025-10-13 21:44:22 +0300

    Exercise 3.1 - 3.6

---
+++ part3/phonebook_backend/index.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const app = express()
+
+const persons = [
+    { id: "1", name: "Arto Hellas", number: "040-123456" },
+    { id: "2", name: "Ada Lovelace", number: "39-44-5323523" },
+    { id: "3", name: "Dan Abramov", number: "12-43-234345" },
+    { id: "4", name: "Mary Poppendieck", number: "39-23-6423122" }
+]
+
+//3.1
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+//3.2
+app.get('/info', (req, res) => {
+    const count = persons.length
+    const date = new Date()
+    res.send(
+        `<div>Phonebook has info for ${count} people</div>
+    <div>${date}</div>`
+    )
+})
+
+//3.3
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    const person = persons.find(p => p.id === id)
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+//3.4
+app.delete('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    const index = persons.findIndex(p => p.id === id)
+    if (index === -1) {
+        return res.status(404).end()
+    }
+    persons.splice(index, 1)
+    res.status(204).end()
+})
+
+//3.5
+app.use(express.json())
+app.post('/api/persons', (req, res) => {
+    const { name, number } = req.body
+
+    if (!name || !number) {
+        return res.status(400).json({ error: 'name or number missing' })
+    }
+
+    const nameExists = persons.some(p => p.name === name)
+    if (nameExists) {
+        return res.status(400).json({ error: 'name must be unique' })
+    }
+
+    const id = (Math.floor(Math.random() * 1e10)).toString()
+    const newPerson = { id, name, number }
+    persons.push(newPerson)
+    res.status(201).json(newPerson)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
