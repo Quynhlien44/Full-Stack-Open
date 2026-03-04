9b8ff151766f707c11a3a6f0eb1210454f7e28ca
Quynhlien44 <quynhlien44@gmail.com>
2026-03-04 17:38:57 +0200

    Exercise 9.8 - 9.11

---
+++ part9/patientor-backend/backend/src/data/diagnoses.ts
@@ -0,0 +1,15 @@
+import { Diagnosis } from '../types';
+
+const diagnoses: Diagnosis[] = [
+  {
+    code: "S03.5",
+    name: "Sprain and strain of joints and ligaments of other and unspecified parts of head",
+    latin: "Distorsio et distractio articulationum et ligamentorum capitis"
+  },
+  {
+    code: "Z57.1",
+    name: "Occupational exposure to radiation"
+  }
+];
+
+export default diagnoses;
